222f57f17a84c790eae53c9cf2c1b1797aeb61e3
Farnood Ensan <141804ensan.f@gmail.com>
2024-07-14 18:22:53 -0700

    Initial commit new

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import firebase from "./FirebaseConfig";
+function App() {
+  return (
+    <div className="App">
+      <div className="title-row">
+        <h1>Firebase Recipes</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
